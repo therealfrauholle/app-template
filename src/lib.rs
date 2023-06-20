17aeb1c57b0a6e8df07793cdc72adc004a97478a
Finn Hermeling <finn.hermeling@elexir.eu>
2023-06-20 16:45:24 +0200

    Adapt to usecase with STM32F767

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,7 @@
 
 use defmt_rtt as _; // global logger
 
-// TODO(5) adjust HAL import
-// use some_hal as _; // memory layout
+use stm32f7xx_hal as _; // memory layout
 
 use panic_probe as _;
 
@@ -19,19 +18,5 @@ fn panic() -> ! {
 pub fn exit() -> ! {
     loop {
         cortex_m::asm::bkpt();
-    }
-}
-
-// defmt-test 0.3.0 has the limitation that this `#[tests]` attribute can only be used
-// once within a crate. the module can be in any file but there can only be at most
-// one `#[tests]` module in this library crate
-#[cfg(test)]
-#[defmt_test::tests]
-mod unit_tests {
-    use defmt::assert;
-
-    #[test]
-    fn it_works() {
-        assert!(true)
     }
 }
